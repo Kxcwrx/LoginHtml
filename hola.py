a5a875e32a600f008602b17dc639097acd6fce46
Brayan Zapata <Brayanfha4@gmail.com>
2025-04-03 09:10:39 -0500

    Cambiossss

---
+++ hola.py
@@ -0,0 +1,12 @@
+hola = "Hola Mundoo"
+print(hola)
+
+if hola == "Hola Mundo":
+    print("La variable contiene el saludo correcto.")
+else:
+    print("La variable no contiene el saludo correcto.")
+
+def suma(a, b):
+    return a + b
+suma(5,4)
+print(suma(5,4))
